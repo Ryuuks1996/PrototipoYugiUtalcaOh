1059ae812244c1b61ef6433c91aea1033c7f3706
Ryuuks1996 <44385392+Ryuuks1996@users.noreply.github.com>
2021-05-16 05:50:38 -0400

    Fixed File Write All Bytes

--- YugiUtalcaOh/Assets/Scripts/Card.cs
+++ YugiUtalcaOh/Assets/Scripts/Card.cs
@@ -10,15 +10,13 @@ public class Card : MonoBehaviour
     public Text inputFieldNameText, inputFieldDescriptionText, inputFieldAttackText, inputFieldDefenseText;
     public GameObject UI_Card;
     public Image cardImage, starImage;
-    public GameObject star, image;
-    public Sprite[] starSprites;
+    public GameObject star;
 
 
 
     private string name, description;
     private int type, element, starAmount, attack, defense;
     private bool isCardFlag = false;
-    private int count = 0;
     private string path;
 
     // Start is called before the first frame update
@@ -31,17 +29,13 @@ public class Card : MonoBehaviour
         description = PlayerPrefs.GetString("CardDescription");
         attack = PlayerPrefs.GetInt("CardAttack");
         defense = PlayerPrefs.GetInt("CardDefense");
-        count = PlayerPrefs.GetInt("CardCount");
 
-        if(count > 0)
-        {
-            var imageBytes = File.ReadAllBytes(Application.persistentDataPath + "/Card/" + name + ".PNG");
-            var texture2D = new Texture2D(256, 256);
-            texture2D.LoadRawTextureData(imageBytes);
-            var sprite = Sprite.Create(texture2D, new Rect(0.0f, 0.0f, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f), 100.0f);
-            image.GetComponent<Image>().sprite = sprite;
-            Debug.Log(texture2D.name);
-        }
+        /*var imageBytes = File.ReadAllBytes(Application.persistentDataPath + "/Card/" + name + ".PNG");
+        var texture2D = new Texture2D(256, 256);
+        texture2D.LoadRawTextureData(imageBytes);
+        var sprite = Sprite.Create(texture2D, new Rect(0.0f, 0.0f, texture2D.width, texture2D.height), new Vector2(0.5f, 0.5f), 100.0f);
+        image.GetComponent<Image>().sprite = sprite;
+        Debug.Log(texture2D.name);*/
         
 
     }
@@ -68,12 +62,10 @@ public class Card : MonoBehaviour
         PlayerPrefs.SetString("CardDescription", description);
         PlayerPrefs.SetInt("CardAttack", attack);
         PlayerPrefs.SetInt("CardDefense", defense);
-        count++;
-        PlayerPrefs.SetInt("CardCount", count);
-        //SaveCard();
+        SaveCard();
 
-        starImage.sprite = starSprites[starAmount];
-        image.GetComponent<Image>().sprite = cardImage.sprite;
+        //starImage.sprite = starSprites[starAmount];
+        //image.GetComponent<Image>().sprite = cardImage.sprite;
         
     }
 
@@ -88,8 +80,6 @@ public class Card : MonoBehaviour
         var timeStamp = DateTime.Now.ToString("yyyMMddHHmmssfff");
         File.WriteAllBytes(dirPath + name + ".PNG", bytesArray);
         PlayerPrefs.SetString("DirPath", path);
-
-        isCardFlag = true;
 
     }
 }
