5d560142d9a6fbdadf2fe6bca651146254871c70
Ryuuks1996 <44385392+Ryuuks1996@users.noreply.github.com>
2021-05-16 00:48:54 -0400

    Fix Card Maker (Saving Image and stars)

--- YugiUtalcaOh/Assets/Scripts/Card.cs
+++ YugiUtalcaOh/Assets/Scripts/Card.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +9,9 @@ public class Card : MonoBehaviour
 {
     public Text inputFieldNameText, inputFieldDescriptionText, inputFieldAttackText, inputFieldDefenseText;
     public GameObject UI_Card;
+    public Image cardImage, starImage;
+    public GameObject star, image;
+    public Sprite[] starSprites;
 
     private string name, description;
     private int type, element, starAmount, attack, defense;
@@ -23,7 +28,6 @@ public class Card : MonoBehaviour
         description = PlayerPrefs.GetString("CardDescription");
         attack = PlayerPrefs.GetInt("CardAttack");
         defense = PlayerPrefs.GetInt("CardDefense");
-        Debug.Log(attack);
     }
 
     // Update is called once per frame
@@ -48,7 +52,22 @@ public class Card : MonoBehaviour
         PlayerPrefs.SetString("CardDescription", description);
         PlayerPrefs.SetInt("CardAttack", attack);
         PlayerPrefs.SetInt("CardDefense", defense);
+        SaveCard();
+
+        starImage.sprite = starSprites[starAmount];
+        image.GetComponent<Image>().sprite = cardImage.sprite;
+        
     }
 
+    public void SaveCard()
+    {
+        byte[] bytesArray = cardImage.sprite.texture.EncodeToPNG();
+        var dirPath = Application.persistentDataPath + "/Cards/";
 
+        if (!Directory.Exists(dirPath))
+            Directory.CreateDirectory(dirPath);
+
+        var timeStamp = DateTime.Now.ToString("yyyMMddHHmmssfff");
+        File.WriteAllBytes(dirPath + name + ".PNG", bytesArray);
+    }
 }
--- YugiUtalcaOh/Assets/Scripts/ImageUploadController.cs
+++ YugiUtalcaOh/Assets/Scripts/ImageUploadController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System;
 
-public class ImageUploadController : MonoBehaviour 
+public class ImageUploadController : MonoBehaviour
 {
     public Image cardImage;
     //private Image cardImage;
@@ -22,7 +24,7 @@ public class ImageUploadController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnClickUploadImageButton()
@@ -52,21 +54,5 @@ public class ImageUploadController : MonoBehaviour
     {
         mySprite = Sprite.Create(theimage, new Rect(0.0f, 0.0f, theimage.width, theimage.height), new Vector2(0.5f, 0.5f), 100.0f);
         cardImage.sprite = mySprite;
-        /*var form = new WWWForm();
-        form.AddBinaryData("imagedata", theimage.EncodeToPNG(), "uploadedimage");
-
-        UnityWebRequestAsyncOperation www = UnityWebRequest.Post("https://www.myrandomserver.com/uploadimage.php", form).SendWebRequest();
-
-        yield return www;
-
-        if (www.webRequest.isHttpError || www.webRequest.isNetworkError)
-        {
-            Debug.Log("Error: " + www.webRequest.error);
-        }
-        else
-        {
-            Debug.Log("Done!");
-        }*/
     }
-
 }
